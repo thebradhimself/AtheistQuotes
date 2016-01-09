b000e5eeb53dcf4d83dd90dabc5c2c226074a4ca
thebradhimself <Bradley.Hintze@gmail.com>
2016-01-09 12:24:04 -0700

    styling

--- app/assets/javascripts/components/QuotesBox.js.jsx
+++ app/assets/javascripts/components/QuotesBox.js.jsx
@@ -97,7 +97,7 @@ class QuotesBox extends React.Component{
             </div>
           </div>
             <a href="https://itunes.apple.com/us/app/atheist-quotes./id392507328?mt=8"><img className="app-buttons" src="http://www.atheist-quotes.com/assets/download_on_app_store_badge-cdd21a520305761890b1da6e15d6151e41d57064668b0deee6bef30e714bc4c9.png"/></a>
-            <a href="https://play.google.com/store/apps/details?id=com.leftblank.atheistquotes"><img className="app-buttons" src="http://www.atheist-quotes.com/assets/get-it-on-google-play-583b7d95966b951ab801b5d4651ed5eca6663e749dceabacc992b3546b2f46ef.png"/></a>
+            <a href="https://play.google.com/store/apps/details?id=com.leftblank.atheistquotes"><img className="app-buttons2" src="http://www.atheist-quotes.com/assets/get-it-on-google-play-583b7d95966b951ab801b5d4651ed5eca6663e749dceabacc992b3546b2f46ef.png"/></a>
         </div>
         <div className="center-align col s12 m3">
           <div className="btn btn-success grey darken-1 shadows fix-that-width" onClick={this.previousQuote}>Previous</div>
