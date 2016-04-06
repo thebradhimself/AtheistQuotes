52b880fc0704082f0dc2e398556a7b189a8e3f76
thebradhimself <Bradley.Hintze@gmail.com>
2016-04-06 11:57:23 -0600

    rack cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,12 +13,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-      origins ENV['ASSET_HOST']
-      resource '*', :headers => :any, :methods => [:get, :options]
-    end
-  end
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
