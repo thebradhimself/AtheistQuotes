17036f0becdb5dbff443c2ac9f9e95e5d533380e
Bradley Hintze <bradley.hintze@gmail.com>
2017-09-19 21:28:19 -0600

    changes

--- app/assets/javascripts/components/Favorites.js.jsx
+++ app/assets/javascripts/components/Favorites.js.jsx
@@ -1,34 +1,34 @@
-class Favorites extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {favorites: this.props.favorites};
-    this.removeFavorite = this.removeFavorite.bind(this);
-  }
-
-  removeFavorite(quoteId){
-    $.ajax({
-      url: '/removeFavorite',
-      type: 'GET',
-      data: {id: quoteId}
-    }).success(data =>{
-      this.setState({favorites: data.favorites});
-    });
-  }
-
-
-
-  render(){
-
-    let favorites = this.state.favorites.map(fav => {
-      return(<Favorite key={`fav-${fav.id}`} favorite={fav} removeFavorite={this.removeFavorite} />);
-    });
-    return(<div>
-            <table className="striped">
-              <tbody>
-                { favorites }
-              </tbody>
-            </table>
-           </div>);
-
-  }
-}
+// class Favorites extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {favorites: this.props.favorites};
+//     this.removeFavorite = this.removeFavorite.bind(this);
+//   }
+//
+//   removeFavorite(quoteId){
+//     $.ajax({
+//       url: '/removeFavorite',
+//       type: 'GET',
+//       data: {id: quoteId}
+//     }).success(data =>{
+//       this.setState({favorites: data.favorites});
+//     });
+//   }
+//
+//
+//
+//   render(){
+//
+//     let favorites = this.state.favorites.map(fav => {
+//       return(<Favorite key={`fav-${fav.id}`} favorite={fav} removeFavorite={this.removeFavorite} />);
+//     });
+//     return(<div>
+//             <table className="striped">
+//               <tbody>
+//                 { favorites }
+//               </tbody>
+//             </table>
+//            </div>);
+//
+//   }
+// }
